953fb2d67f7b67ee5c231f27d3152e129efc07d3
Kevin Brick <kevbrick@gmail.com>
2022-02-03 07:18:51 -0500

    Update getAccessoryFiles.sh

--- getAccessoryFiles.sh
+++ getAccessoryFiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-wget https://hpc.nih.gov/~brickkm/meioticReplicationData/PrattoEtAl_accessoryFiles.tar.gz 
+#wget https://hpc.nih.gov/~brickkm/meioticReplicationData/PrattoEtAl_accessoryFiles.tar.gz 
+# Published file is hosted on Zenodo
+wget https://zenodo.org/record/4590899/files/PrattoEtAl_accessoryFiles.tar.gz
 
 tar -zxvf PrattoEtAl_accessoryFiles.tar.gz
 
